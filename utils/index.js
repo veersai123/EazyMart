4380aa0c845820cb1527812245c621a95fa340d3
gyi89 <saasdsds068@gmail.com>
2024-04-03 23:37:58 +0530

    intial commit

---
+++ utils/index.js
@@ -0,0 +1,43 @@
+export const navOptions = [
+    {
+      id: "home",
+      label: "Home",
+      path: "/",
+    },
+    {
+      id: "listing",
+      label: "All Products",
+      path: "/product/listing/all-products",
+    },
+    {
+      id: "listingMen",
+      label: "Men",
+      path: "/product/listing/men",
+    },
+    {
+      id: "listingWomen",
+      label: "Women",
+      path: "/product/listing/women",
+    },
+    {
+      id: "listingKids",
+      label: "kids",
+      path: "/product/listing/kids",
+    },
+  ];
+  export const adminNavOptions = [
+    {
+      id: "adminListing",
+      label: "Manage All Products",
+      path: "/admin-view/all-products",
+    },
+    {
+      id: "adminNewProduct",
+      label: "Add New Product",
+      path: "/admin-view/add-product",
+    },
+  ];
+export const styles = {
+    button: "mt-1.5 inline-block bg-gray-800 px-4 py-2 text-sm font-medium text-white rounded-md",
+  }
+  
